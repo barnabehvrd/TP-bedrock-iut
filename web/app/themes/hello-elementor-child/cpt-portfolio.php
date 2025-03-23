5bfd6b40a8a877787458a72aaefdbf30bf5d9e34
v0ltis <voltis.discord@gmail.com>
2025-03-23 13:49:59 +0100

    Add rewrite rules for portfolio custom post type

--- web/app/themes/hello-elementor-child/cpt-portfolio.php
+++ web/app/themes/hello-elementor-child/cpt-portfolio.php
@@ -50,6 +50,7 @@ function custom_post_type() {
 		'exclude_from_search'   => false,
 		'publicly_queryable'    => true,
 		'capability_type'       => 'page',
+		'rewrite'               => array( 'slug' => 'portfolio' ),
 	);
 	register_post_type( 'post_type', $args);
 
