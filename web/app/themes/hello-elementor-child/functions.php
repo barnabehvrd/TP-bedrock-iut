8277a55acd5073d50d1d9cfa0e7896a016b342d8
v0ltis <voltis.discord@gmail.com>
2025-03-23 15:22:29 +0100

    Add dark theme styles and update stylesheet enqueuing

--- web/app/themes/hello-elementor-child/functions.php
+++ web/app/themes/hello-elementor-child/functions.php
@@ -3,15 +3,22 @@
 function mon_theme_enfant_enqueue_styles()
 {
 
-    $parent_style = 'parent-style'; // Ceci doit correspondre au handle du thème parent (vérifiez le functions.php du thème parent si vous n'êtes pas sûr)
+    $parent_style = 'parent-style';
 
-    wp_enqueue_style($parent_style, get_template_directory_uri().'/style.css'); // Enfile le style du thème parent
+    wp_enqueue_style($parent_style, get_template_directory_uri().'/style.css');
 
-    wp_enqueue_style('child-style', // Handle pour le style du thème enfant
-        get_stylesheet_directory_uri().'/style.css', // Chemin vers le style.css du thème enfant
-        [$parent_style], // Dépendance : s'assurer que le style parent est chargé avant
-        wp_get_theme()->get('Version') // Version du thème enfant
+    wp_enqueue_style('child-style',
+        get_stylesheet_directory_uri().'/style.css',
+        [$parent_style],
+        wp_get_theme()->get('Version')
     );
+
+	wp_enqueue_style('dark-theme',
+		get_stylesheet_directory_uri().'/dark-style.css',
+		['child-style'],
+		wp_get_theme()->get('Version') . '.' . time() // Ajouter timestamp pour éviter la mise en cache
+	);
+
 }
 add_action('wp_enqueue_scripts', 'mon_theme_enfant_enqueue_styles');
 
